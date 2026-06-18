2cd44f59dc8bb9ce554f93d2dc4631d045dbdee3
Thomas Brandner <brandner.thomas@me.com>
2026-06-18 19:18:26 +0200

    add demo test case

---
+++ src/test/java/at/htlklu/pos/springvehiclelog/DemoTest.java
@@ -0,0 +1,11 @@
+package at.htlklu.pos.springvehiclelog;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+    @Test
+    public void testDemo() {
+        Assertions.assertTrue(true);
+    }
+}
